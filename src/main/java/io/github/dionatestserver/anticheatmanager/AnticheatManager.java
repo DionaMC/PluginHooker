b1d086421d83b4eeeaafde3f6cc7c7b242116c3a
Loyisa <894857715@qq.com>
2022-05-09 14:24:53 +0800

    init

---
+++ src/main/java/io/github/dionatestserver/anticheatmanager/AnticheatManager.java
@@ -0,0 +1,17 @@
+package io.github.dionatestserver.anticheatmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnticheatManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
