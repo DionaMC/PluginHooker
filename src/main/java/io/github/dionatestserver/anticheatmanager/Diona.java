b7a2342da376971ce395246268f0e129e06c610a
Hellxd <hellxd@qq.com>
2022-05-14 20:29:58 +0800

    refactor: 移动DionaPlayer相关类到player包

--- src/main/java/io/github/dionatestserver/anticheatmanager/Diona.java
+++ src/main/java/io/github/dionatestserver/anticheatmanager/Diona.java
@@ -1,11 +1,12 @@
 package io.github.dionatestserver.anticheatmanager;
 
 import io.github.dionatestserver.anticheatmanager.anticheat.AnticheatManager;
-import io.github.dionatestserver.anticheatmanager.anticheat.PlayerManager;
 import io.github.dionatestserver.anticheatmanager.hook.HookerManager;
 import io.github.dionatestserver.anticheatmanager.listeners.PlayerListener;
+import io.github.dionatestserver.anticheatmanager.player.PlayerManager;
 import lombok.Getter;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
@@ -15,7 +16,7 @@ public final class Diona extends JavaPlugin {
     private static final HookerManager hookerManager;
 
     static {
-        (hookerManager = new HookerManager()).init();
+        hookerManager = new HookerManager();
         hookerManager.injectEventHandler();
     }
 
